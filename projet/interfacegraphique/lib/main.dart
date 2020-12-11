dfc05e4e56513817328ae4d4cb937ac1fea16ba7
franck1999-30 <christinnguekam@gmail.com>
2020-12-11 12:03:08 -0500

    new commit :whale:

--- projet/interfacegraphique/lib/main.dart
+++ projet/interfacegraphique/lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Nav2App());
+  runApp(HeroApp());
 }
 
-class Nav2App extends StatelessWidget {
+class HeroApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      home: MainScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class MainScreen extends StatelessWidget {
   final myController = TextEditingController();
   @override
   Widget build(BuildContext context) {
@@ -22,10 +22,13 @@ class HomeScreen extends StatelessWidget {
       body: Center(
           child: Column(
         children: [
-          Image.network(
-            "https://www.apcm.ca/wp-content/uploads/2016/09/Colle%CC%80geBore%CC%81al3.jpeg",
-            width: 300,
-            height: 200,
+          Hero(
+            tag: 'imageHero',
+            child: Image.network(
+              "https://www.apcm.ca/wp-content/uploads/2016/09/Colle%CC%80geBore%CC%81al3.jpeg",
+              width: 300,
+              height: 200,
+            ),
           ),
           Padding(
             padding: const EdgeInsets.only(top: 80.0),
@@ -48,7 +51,7 @@ class HomeScreen extends StatelessWidget {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) {
-                      return DetailScreen();
+                      return DetailS1creen();
                     }),
                   );
                 },
@@ -79,22 +82,90 @@ child: FlatButton(
         ),
         */
 
-class DetailScreen extends StatelessWidget {
+class DetailS1creen extends StatelessWidget {
+  final myController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: FlatButton(
-          child: Column(
-            children: [
-              Text('Deuxieme Ecran'),
-              Text('Pop!'),
-            ],
-          ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+        child: Column(
+          children: [
+            Hero(
+              tag: 'imageHero',
+              child: Image.network(
+                "https://www.apcm.ca/wp-content/uploads/2016/09/Colle%CC%80geBore%CC%81al3.jpeg",
+                width: 300,
+                height: 200,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 80.0),
+              child: Text("Bienvenue !"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                  "Pour accéder au campus aujourd'hui, tu dois completer le questionnaire."),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: FlatButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) {
+                        return DetailS1creen();
+                      }),
+                    );
+                  },
+                  child: Text("Completer le questionnaire")),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Detail2Screen extends StatelessWidget {
+  final myController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [
+            Hero(
+              tag: 'imageHero',
+              child: Image.network(
+                "https://www.apcm.ca/wp-content/uploads/2016/09/Colle%CC%80geBore%CC%81al3.jpeg",
+                width: 300,
+                height: 200,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 80.0),
+              child: Text("formulaire CoviD-19 pour "),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 80.0),
+              child: Text("Date 2020-11-25 "),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text("Aujourd'hui, je me sens :"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: FlatButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("Completer le questionnaire")),
+            )
+          ],
         ),
       ),
     );
